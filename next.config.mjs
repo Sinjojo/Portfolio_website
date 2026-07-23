d1c73311ea46460126a677a5ad9f28656107723e
Sinjojo <salankrmx@gmail.com>
2026-07-23 18:37:27 +0530

    solved the nextjs old version problem

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
+// @ts-check
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+
+// Initialize the Cloudflare adapter for local `next dev`
+initOpenNextCloudflareForDev();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
